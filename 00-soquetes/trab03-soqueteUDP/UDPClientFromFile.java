5a40b84775a4f7b56b15e00e428ea78258558ead
William Pereira <willpereirabr@gmail.com>
2017-03-16 10:59:19 -0300

    file read add and message from nodes lenght fixed

---
+++ 00-soquetes/trab03-soqueteUDP/UDPClientFromFile.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.net.*;
+
+class UDPClientFromFile {
+
+    public static void main(String args[]) throws Exception {
+        FileInputStream in = null;
+        BufferedInputStream bis = null;
+        DataInputStream dis = null;
+        String sentence = "";
+        try {
+            in = new FileInputStream("text.txt");
+            DatagramSocket clientSocket = new DatagramSocket();
+            InetAddress IPAddress
+                    = InetAddress.getByName("localhost");
+            byte[] sendData = new byte[65507];
+            byte[] receiveData = new byte[65507];
+            int count;
+            while ((count = in.read()) != -1){
+              sentence += Character.toString((char)count);
+            }
+            // NOTE: Next lines is for test purposes
+            System.out.print(sentence);
+            System.out.println("---------------------------------------------------------");
+            sendData = sentence.getBytes();
+            System.out.println(sendData.length);
+            DatagramPacket sendPacket
+                    = new DatagramPacket(sendData, sendData.length,
+                            IPAddress, 9876);
+            clientSocket.send(sendPacket);
+            DatagramPacket receivePacket
+                    = new DatagramPacket(receiveData,
+                            receiveData.length);
+            clientSocket.receive(receivePacket);
+            String modifiedSentence
+                    = new String(receivePacket.getData());
+            System.out.println("FROM SERVER:"
+                    + modifiedSentence);
+            clientSocket.close();
+      }catch (FileNotFoundException e) {
+        e.printStackTrace();
+      }catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
+}
