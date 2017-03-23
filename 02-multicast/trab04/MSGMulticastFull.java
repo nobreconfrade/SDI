ed0a00005b9fa6faefb6578ed6d62fda7b84ab89
William Pereira <willpereirabr@gmail.com>
2017-03-23 09:56:56 -0300

    work in progress

--- 02-multicast/trab04/MSGMulticastFull.java
+++ 02-multicast/trab04/MSGMulticastFull.java
@@ -1,36 +1,33 @@
 import java.io.*;
 import java.net.*;
-/**
- * @author lycog
- */
-public class MulticastSender {
+
+public class MSGMulticastFull extends Thread{
   public static void main(String[] args) {
     DatagramSocket socket = null;
     DatagramPacket outPacket = null;
-    is = new DataInputStream(clientSocket.getInputStream());
+    BufferedReader buffer=new BufferedReader(new InputStreamReader(System.in));
+    String nickname;
+    String msg;
     byte[] outBuf;
     final int PORT = 8888;
 
     try {
       socket = new DatagramSocket();
       long counter = 0;
-      String msg;
       System.out.println("Escreva seu nome:");
-      String name = is.readLine().trim();
+      nickname = buffer.readLine().trim();
 
       while (true) {
         System.out.println("Escreva sua mensagem:");
-        msg = is.readLine().trim();
+        msg = buffer.readLine().trim();
         counter++;
         outBuf = msg.getBytes();
-
         //Send to multicast IP address and port
         InetAddress address = InetAddress.getByName("224.2.2.3");
         outPacket = new DatagramPacket(outBuf, outBuf.length, address, PORT);
 
         socket.send(outPacket);
 
-        System.out.println("Server sends : " + msg);
         try {
           Thread.sleep(500);
         } catch (InterruptedException ie) {
