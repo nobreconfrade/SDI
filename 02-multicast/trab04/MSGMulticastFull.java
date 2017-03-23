15d16da1666beced2b49d14c165af085a7ff5de6
William Pereira <willpereirabr@gmail.com>
2017-03-23 11:35:19 -0300

    thread add, chat fully working

--- 02-multicast/trab04/MSGMulticastFull.java
+++ 02-multicast/trab04/MSGMulticastFull.java
@@ -1,9 +1,11 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
+import java.lang.String;
 
-public class MSGMulticastFull extends Thread{
+public class MSGMulticastFull{
   public static void main(String[] args) {
-    DatagramSocket socket = null;
+    DatagramSocket socketsend = null;
     DatagramPacket outPacket = null;
     BufferedReader buffer=new BufferedReader(new InputStreamReader(System.in));
     String nickname;
@@ -12,29 +14,60 @@ public class MSGMulticastFull extends Thread{
     final int PORT = 8888;
 
     try {
-      socket = new DatagramSocket();
       long counter = 0;
       System.out.println("Escreva seu nome:");
       nickname = buffer.readLine().trim();
-
+      InetAddress address = InetAddress.getByName("224.2.2.3");
+      socketsend = new DatagramSocket();
+      // NOTE: Receivers end in next line
+      receive(nickname);
+      System.out.println("ONLINE!");
       while (true) {
-        System.out.println("Escreva sua mensagem:");
         msg = buffer.readLine().trim();
+        msg = nickname+":"+msg;
         counter++;
         outBuf = msg.getBytes();
         //Send to multicast IP address and port
-        InetAddress address = InetAddress.getByName("224.2.2.3");
         outPacket = new DatagramPacket(outBuf, outBuf.length, address, PORT);
+        socketsend.send(outPacket);
 
-        socket.send(outPacket);
 
-        try {
-          Thread.sleep(500);
-        } catch (InterruptedException ie) {
-        }
       }
     } catch (IOException ioe) {
       System.out.println(ioe);
     }
+  }
+
+  private static void receive(String nickname){
+    new Thread(new Runnable(){
+      @Override
+      public void run(){
+        try{
+          while(true){
+            MulticastSocket socketreceive = null;
+            DatagramPacket inPacket = null;
+            byte[] inBuf = new byte[256];
+            socketreceive = new MulticastSocket(8888);
+            InetAddress address = InetAddress.getByName("224.2.2.3");
+            String msg;
+
+            socketreceive.joinGroup(address);
+            inPacket = new DatagramPacket(inBuf, inBuf.length);
+            socketreceive.receive(inPacket);
+            msg = new String(inBuf, 0, inPacket.getLength());
+            String[] finalmsg = msg.split(":");
+            if (!finalmsg[0].equals(nickname)) {
+              System.out.println(finalmsg[0] + ": " + finalmsg[1]);
+            }
+            try {
+              Thread.sleep(500);
+            } catch (InterruptedException ie) {
+            }
+          }
+        } catch (IOException ioe){
+          System.out.println(ioe);
+        }
+      }
+    }).start();
   }
 }
