6d7755217503a22801ebcd86f5af981082949611
William Pereira <willpereirabr@gmail.com>
2017-03-23 09:56:20 -0300

    trying some thread stuff

---
+++ 02-multicast/trab04/something.java
@@ -0,0 +1,7 @@
+public class something extends Thread{
+  public static void main(String[] args){
+    MSGMulticastReceiver receive = new MSGMulticastReceiver();
+    MSGMulticastSender sender = new MSGMulticastSender();
+    
+  }
+}
