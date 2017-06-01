3e99bb3397458262030a308f7ede0b5fa026ccab
William Pereira <willpereirabr@gmail.com>
2017-05-31 21:31:28 -0300

    output image working?

--- trabfinal/test.java
+++ trabfinal/test.java
@@ -4,15 +4,15 @@ class test{
     public static void main(String args[]) throws Exception {
         FileInputStream in = null;
 		in = new FileInputStream("tests/test1.jpg");
-		
+
 		// FILE *file = fopen("tests/test1.jpg");
 		// FILE *fout = fopen("output.jpg");
 
 		byte[] buffer = new byte[65507];
 		int count = 0;
 		long size = in.getChannel().size();
+    int i = 0;
 		while ((count += in.read(buffer, 0, 65507)) < size){
-			int i = 0;
 			FileOutputStream out = new FileOutputStream("output"+i+".jpg");
         	out.write(buffer);
         	out.close();
