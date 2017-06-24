9d921e265f32e72a60ed87fb306ad295fcc710f1
Kronossaurus <brvictor.chess@hotmail.com>
2017-06-24 19:55:41 -0300

    Reconstrução

--- trabfinal/reconstrucao.java
+++ trabfinal/reconstrucao.java
@@ -1,16 +1,35 @@
 import java.io.*;
+import java.util.ArrayList;
 
 class reconstrucao{
     public static void main(String args[]) throws Exception {
-    	int chunks = 3;
+    	FileInputStream file = null;
+        Metadado meta = new Metadado();
+
+        if(args.length == 0){
+            System.out.println("Uso: reconstrucao [metadado]");
+            System.exit(0);
+        }
+        file = new FileInputStream(args[0]);//checar .sdi?
+        //leitura
+        byte[] bytes = new byte[(int)file.getChannel().size()];
+        file.read(bytes);
+        ByteArrayInputStream bos = new ByteArrayInputStream(bytes);
+        ObjectInput in  = new ObjectInputStream(bos);
+        meta.nome       = in.readUTF();
+        meta.size       = in.readLong();
+        meta.n_chunks   = in.readInt();
+        meta.vetor_dados = (ArrayList <Dados>) in.readObject();
+        //fimleitura
     	FileOutputStream out = new FileOutputStream("reconstruido.jpg");
-    	for(int i=0; i<chunks; i++){
-        	FileInputStream in = new FileInputStream("output"+i+".jpg");
-        	long size = in.getChannel().size();
+        FileInputStream chunk;
+    	for(Dados it: meta.vetor_dados){
+            chunk = new FileInputStream(String.valueOf(it.hash_chunk)+".chunk");
+        	long size = chunk.getChannel().size();
 			byte[] buffer = new byte[65507];
-			in.read(buffer, 0, (int)size);
+			chunk.read(buffer, 0, (int)size);
 			out.write(buffer, 0, (int)size);
-        	in.close();
+        	chunk.close();
     	}
     	out.close();
     }
