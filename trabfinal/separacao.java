0365029c80675bc4b90abb4de1409af90e4dba68
Kronossaurus <brvictor.chess@hotmail.com>
2017-06-24 19:26:41 -0300

    I/O and makefile

--- trabfinal/separacao.java
+++ trabfinal/separacao.java
@@ -1,12 +1,12 @@
 import java.io.*;
 import java.util.ArrayList;
 
-class Dados{
+class Dados implements Serializable{
     int     hash_chunk;
 
     ArrayList <String>  vetor_bd_chunk = new ArrayList <String>();
 }
-class Metadado{
+class Metadado implements Serializable{
     String  nome;
     long    size;
     int     n_chunks;
@@ -15,11 +15,35 @@ class Metadado{
 
     public void save() throws Exception{
         FileOutputStream file = new FileOutputStream(this.nome+".sdi");
-        PrintStream printStream = new PrintStream(file);
-        printStream.println(this.nome);
-        printStream.println(this.size);
-        file.write(this.n_chunks);
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutput out = new ObjectOutputStream(bos);
+
+        out.writeUTF(this.nome);
+        out.writeLong(this.size);
+        out.writeInt(this.n_chunks);
+        out.writeObject(this.vetor_dados);
+        byte[] buf = bos.toByteArray();
+        file.write(buf);
         file.close();
+    }
+
+    public void debug() throws Exception{
+        FileInputStream file = new FileInputStream(this.nome+".sdi");
+        byte[] bytes = new byte[(int)file.getChannel().size()];
+        file.read(bytes);
+        ByteArrayInputStream bos = new ByteArrayInputStream(bytes);
+        ObjectInput in = new ObjectInputStream(bos);
+        String nome = in.readUTF();
+        System.out.println(nome);
+        long size = in.readLong();
+        System.out.println(size);
+        int n_chunks = in.readInt();
+        System.out.println(n_chunks);
+        ArrayList <Dados> list = new ArrayList <Dados>();
+        // in.readObject(list);
+        list = (ArrayList <Dados>) in.readObject();
+        for(Dados it: list)
+            System.out.println(it.hash_chunk);
     }
 }
 class separacao{
@@ -49,6 +73,7 @@ class separacao{
             }
             Dados aux = new Dados();
             aux.hash_chunk = hashvalue;
+            aux.vetor_bd_chunk.add("teste");
             meta.vetor_dados.add(aux);
             // System.out.println(hashvalue);
             FileOutputStream out = new FileOutputStream(String.valueOf(hashvalue) + ".chunk");
@@ -62,7 +87,7 @@ class separacao{
         meta.size       = size;
         meta.n_chunks   = i;
         meta.save();
-
+        // meta.debug();
         in.close();
 	}
 }
