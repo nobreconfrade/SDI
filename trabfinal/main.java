777e2497859b2f52cd2eb7a573811ba0314b9ce4
Kronossaurus <brvictor.chess@hotmail.com>
2017-06-27 04:06:57 -0300

    main

--- trabfinal/main.java
+++ trabfinal/main.java
@@ -55,7 +55,7 @@ class Metadado implements Serializable{
 }
 
 class main{
-    public static void separacao(String args) throws Exception {
+    public static String separacao(String args) throws Exception {
         FileInputStream in = null;
         // if(args.length == 0){
         //     System.out.println("Uso: separacao [arquivo]");
@@ -95,8 +95,9 @@ class main{
         meta.size       = size;
         meta.n_chunks   = i;
         meta.save();
-        meta.debug();
+        // meta.debug();
         in.close();
+        return nome+".sdi";
     }
     public static void reconstrucao(String args) throws Exception {
         FileInputStream file = null;
@@ -129,7 +130,134 @@ class main{
         }
         out.close();
     }
+    public static void cliente(String arquivosdi) throws Exception {
+        int serverPort = 3248;
+        FileInputStream listaips,file = null;
+        DataInputStream dis = null;
+        String sentence = "";
+        Metadado meta = new Metadado();
+        file = new FileInputStream(arquivosdi);
+        try {
 
+            byte[] bytes = new byte[(int)file.getChannel().size()];
+            file.read(bytes);
+            ByteArrayInputStream lbos = new ByteArrayInputStream(bytes);
+            ObjectInput in   = new ObjectInputStream(lbos);
+            meta.nome        = in.readUTF();
+            meta.size        = in.readLong();
+            meta.n_chunks    = in.readInt();
+            meta.vetor_dados = (ArrayList <Dados>) in.readObject();
+
+            listaips = new FileInputStream("listaips.txt");
+            int count;
+            while ((count = listaips.read()) != -1){
+              sentence += Character.toString((char)count);
+            }
+            String[] lines = sentence.split(System.getProperty("line.separator"));
+            int enviados = 0;
+            // for(int i=0; i<lines.length && enviados<meta.n_chunks;i++){
+            for(int i=0;enviados<meta.n_chunks;i++){
+                InetAddress teste = InetAddress.getByName(lines[i%lines.length]);
+                if(!teste.isReachable(1000)){
+                  System.out.println("Conexão falhou");
+                  continue;
+                }
+                // Socket server = new Socket (lines[i%lines.length],serverPort);
+                Socket server = new Socket ("localhost",serverPort);
+                DataOutputStream paraservidor = new DataOutputStream(server.getOutputStream());
+                paraservidor.writeBytes("send_"+String.valueOf(meta.vetor_dados.get(enviados).hash_chunk));
+                System.out.println("send_"+meta.vetor_dados.get(enviados).hash_chunk);
+                paraservidor.flush();
+                // paraservidor.close();
+                // server.close();
+                // server = new Socket (lines[i%lines.length],serverPort);
+                BufferedOutputStream bufferservidor = new BufferedOutputStream(server.getOutputStream());
+                File meuchunk = new File("chunks/"+meta.vetor_dados.get(enviados).hash_chunk+".chunk");
+                byte[] mybytearray = new byte[(int) meuchunk.length()];
+                FileInputStream fis = new FileInputStream(meuchunk);
+                BufferedInputStream bis = new BufferedInputStream(fis);
+                bis.read(mybytearray, 0, mybytearray.length);
+                bufferservidor.write(mybytearray, 0, mybytearray.length);
+                bufferservidor.flush();
+                bufferservidor.close();
+                server.close();
+                meta.vetor_dados.get(enviados).vetor_bd_chunk.add(lines[i%lines.length]);
+                meta.save();
+                // System.out.println(meta.vetor_dados.get(enviados).vetor_bd_chunk.get(0));
+                enviados++;
+            }
+            if(enviados != meta.n_chunks)
+                System.out.println("Envio falhou");
+            else{
+                System.out.println("Sucesso");
+                for(int i=0;i<meta.n_chunks;i++){
+                    File filel = new File("chunks/"+meta.vetor_dados.get(i).hash_chunk+".chunk");
+                    if (filel.delete()) {
+                        System.out.println(filel.getName() + " foi deletada!");
+                    }
+                }
+            }
+
+            meta.debug();
+            return;
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void servidor() throws Exception {
+        int serverPort = 3248;
+      try{
+        byte[] aByte = new byte[1];
+        int bytesRead;
+        InputStream is = null;
+        ServerSocket servidor = new ServerSocket(serverPort);
+        System.out.println("Servidor ouvindo a porta: "+serverPort);
+        while(true) {
+            Socket cliente = servidor.accept();
+            BufferedReader docliente = new BufferedReader( new InputStreamReader(cliente.getInputStream()));
+            String mensage = docliente.readLine();
+            String msg[] = mensage.split("_");
+            if(msg[0].equals("send")){
+                System.out.println(msg[1]);
+                msg[1] = msg[1].replaceAll("(\\d+).*", "$1");
+                System.out.println(msg[1]);
+                // try {
+                // cliente = servidor.accept();
+                is = cliente.getInputStream();
+                // }catch (IOException ex) {
+                //   System.out.println("Algum problema na segunda fase de conexão.");
+                // }
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                FileOutputStream fos = new FileOutputStream("chunkspassed/"+msg[1]+".chunk");
+                // FileOutputStream fos = new FileOutputStream("chunkspassed/BABANA.chunk");
+                BufferedOutputStream bos = new BufferedOutputStream(fos);
+                bytesRead = is.read(aByte, 0, aByte.length);
+                do {
+                    baos.write(aByte);
+                    bytesRead = is.read(aByte);
+                }while (bytesRead != -1);
+                bos.write(baos.toByteArray());
+                bos.flush();
+                bos.close();
+                cliente.close();
+            }else{
+                System.out.println("quase la! "+msg);
+            }
+
+            System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
+            // ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
+            // saida.flush();
+            // saida.close();
+            // cliente.close();
+            }
+        }
+        catch(Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+    
     public static void main(String args[]) throws Exception {
         System.out.println("Selecione a operação:");
         System.out.println("1 - Separação [arquivo.jpg]");
@@ -154,7 +282,8 @@ class main{
         // System.out.println(arquivo);
 
         if (option == 1) {
-            separacao(arquivo);
+            String arquivosdi = separacao(arquivo);
+            cliente(arquivosdi);
         }
         else if (option == 2){
             reconstrucao(arquivo);
