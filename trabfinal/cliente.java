94645b6b9e230ad3eadd8d0ed37386a12ce30a07
William Pereira <willpereirabr@gmail.com>
2017-06-26 23:57:02 -0300

    plus ultra

---
+++ trabfinal/cliente.java
@@ -0,0 +1,87 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+class cliente {
+  private final static int serverPort = 3248;
+
+    public static void main(String args[]) throws Exception {
+        FileInputStream listaips,file = null;
+        BufferedInputStream bis = null;
+        DataInputStream dis = null;
+        String sentence = "";
+        Metadado meta = new Metadado();
+        file = new FileInputStream(args[0]);
+        try {
+
+            byte[] bytes = new byte[(int)file.getChannel().size()];
+            file.read(bytes);
+            ByteArrayInputStream lbos = new ByteArrayInputStream(bytes);
+            ObjectInput in   = new ObjectInputStream(lbos);
+            meta.nome        = in.readUTF();
+            meta.size        = in.readLong();
+            meta.n_chunks    = in.readInt();
+            meta.vetor_dados = (ArrayList <Dados>) in.readObject();
+
+            listaips = new FileInputStream("listaips.txt");
+            int count;
+            while ((count = listaips.read()) != -1){
+              sentence += Character.toString((char)count);
+            }
+            String[] lines = sentence.split(System.getProperty("line.separator"));
+
+            for(int i=0;i<meta.n_chunks;i++){
+                Socket server = new Socket (lines[i%lines.length],serverPort);
+                PrintWriter output = new PrintWriter(server.getOutputStream());
+                output.print("send");
+                output.flush();
+                output.close();
+            }
+      }catch (FileNotFoundException e) {
+        e.printStackTrace();
+      }catch (IOException e) {
+        e.printStackTrace();
+      }
+  }
+}
+
+
+            // for(int i=0;i<meta.n_chunks;i++){
+              // ServerSocket welcomeSocket = null;
+              // Socket connectionSocket = null;
+              // BufferedOutputStream outToClient = null;
+              //
+              //  try {
+              //      welcomeSocket = new ServerSocket(3248);
+              //      connectionSocket = welcomeSocket.accept();
+              //      outToClient = new BufferedOutputStream(connectionSocket.getOutputStream());
+              //  } catch (IOException ex) {
+              //      // Do exception handling
+              //  }
+              //
+              //  if (outToClient != null) {
+              //      File myFile = new File("chunks/"+meta.vetor_dados.get(i).hash_chunk+".chunk");
+              //      byte[] mybytearray = new byte[(int) myFile.length()];
+              //
+              //      FileInputStream fis = null;
+              //
+              //      try {
+              //          fis = new FileInputStream(myFile);
+              //      } catch (FileNotFoundException ex) {
+              //          System.out.println("Não encontrou o chunk na maquina local");
+              //      }
+              //      BufferedInputStream bis = new BufferedInputStream(fis);
+              //
+              //      try {
+              //          bis.read(mybytearray, 0, mybytearray.length);
+              //          outToClient.write(mybytearray, 0, mybytearray.length);
+              //          outToClient.flush();
+              //          outToClient.close();
+              //          connectionSocket.close();
+              //
+              //          // File sent, exit the main method
+              //          return;
+              //      } catch (IOException ex) {
+              //          System.out.println("Não conseguiu enviar o arquivo");
+              //      }
+// TODO: Recebimento abaixo
---
+++ trabfinal/servidor.java
@@ -0,0 +1,30 @@
+
+import java.io.*;
+import java.net.*;
+
+class servidor {
+private final static int serverPort = 3248;
+    public static void main(String args[]) throws Exception {
+      try{
+        ServerSocket servidor = new ServerSocket(serverPort);
+        System.out.println("Servidor ouvindo a porta: "+serverPort);
+        while(true) {
+          Socket cliente = servidor.accept();
+          BufferedReader msg = new BufferedReader( new InputStreamReader(cliente.getInputStream()));
+          if(msg.equals("send")){
+            System.out.println("opa, vamo que vamo");
+          }else{
+            System.out.println("quase la"+msg);
+          }
+          System.out.println("Cliente conectado: " + cliente.getInetAddress().getHostAddress());
+          ObjectOutputStream saida = new ObjectOutputStream(cliente.getOutputStream());
+          saida.flush();
+          saida.close();
+          cliente.close();
+        }
+      }
+    catch(Exception e) {
+       System.out.println("Erro: " + e.getMessage());
+    }
+  }
+}
